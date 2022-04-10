fd47cc45038891fb9da06d8702bdcf0562622863
Daniel Hernandez <dan.alejandro.hrnndz@gmail.com>
2022-04-10 16:10:16 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 let prevDisplay = document.getElementById("display_previous");
 let currDisplay = document.getElementById("display_current");
-
 let btnNum = document.querySelectorAll(".btn_num");
 let btnOp = document.querySelectorAll(".btn_op");
 let btnClear = document.querySelector(".btn_c");
@@ -12,21 +11,21 @@ var previousOp = "";
 var currentOp = "";
 var operation = undefined;
 
+var isResult = false
+
 
 
 btnNum.forEach(button => {
     button.addEventListener("click", () => {
-        if (button.innerHTML === "."){
-            if 
-        }
         addNumber(button.innerHTML);
+        updateDisplay()
     })
 })
 
 btnOp.forEach(button => {
     button.addEventListener("click", () => {
-        opSelection();
         addOperand(button.innerHTML);
+        updateDisplay()
     })
 })
 
@@ -35,39 +34,91 @@ btnClear.addEventListener("click" , () => {
 })
 
 btnDel.addEventListener("click", () => {
-    currDisplay.value = currDisplay.value.slice(0, -1);
+    currentOp = currentOp.slice(0, -1);
+    updateDisplay()
 })
 
 btnClear.addEventListener("click" , () => {
     clear()
+    updateDisplay()
 })
 
 btnEqual.addEventListener("click", () => {
-    calculate()
-    displaySwitch()
+    if (isResult === false) {
+        calculate()
+        updateDisplay()
+    }
+    else {
+        return
+    }
 })
 
-updateDisplay = () => {
-    currDisplay = currentOp;
-    prevDisplay = previousOp;
+clear = () => {
+    previousOp = "";
+    currentOp = "";
+    operation = undefined
 }
 
-clear = () => {
-    prevDisplay.value = "";
-    currDisplay.value = "";
+updateDisplay = () => {
+    currDisplay.value = currentOp;
+    prevDisplay.value = previousOp;
 }
 
 addNumber = (num) => {
-    currDisplay.value += num;
+    if (num === "." && currentOp.includes(".")) {
+        return
+    }
+    if (num === "0" && currentOp == "") {
+        return
+    }
+    currentOp += num;
+
+    isResult = false;
 }
 
 addOperand = (oper) => {
-    previousOp = currentOp
-    currentOp = "";
+    if (isResult === true) {
+        previousOp = currentOp
+        currentOp = ""
+    }
+    currentOp += oper;
+    previousOp += currentOp;
+    currentOp = ""
+    operation = oper
 }
 
-// calculate = () => {
-    
-// }
+calculate = () => {
+    var calculation;
+
+    const previous = parseFloat(previousOp);
+    const current = parseFloat(currentOp);
+
+
+    if(isNaN(previous) || isNaN(current)) {
+        return
+    }
+
+
+    switch(operation){
+        case "+":
+            calculation = previous + current;
+            break
+        case "-":
+            calculation = previous - current;
+            break
+        case "X":
+            calculation = previous * current;
+            break
+        case "/":
+            calculation = previous / current;
+            break
+        default:
+            return
+    }
+    previousOp += currentOp
+    currentOp = calculation;
+
+    isResult = true;
+}
 
 
