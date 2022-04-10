e33b81660876a673cc7b4483254a7950bf278a4b
Daniel Hernandez <dan.alejandro.hrnndz@gmail.com>
2022-04-10 13:28:27 -0300

    update

--- script.js
+++ script.js
@@ -1,17 +1,73 @@
 
-const prevDisplay = document.getElementById('display_previous');
-const currDisplay = document.getElementById('display_current');
-const btn = Array.from(document.getElementsByClassName('btn'));
+let prevDisplay = document.getElementById("display_previous");
+let currDisplay = document.getElementById("display_current");
+
+let btnNum = document.querySelectorAll(".btn_num");
+let btnOp = document.querySelectorAll(".btn_op");
+let btnClear = document.querySelector(".btn_c");
+let btnDel = document.querySelector(".btn_del");
+let btnEqual = document.querySelector(".btn_equal");
+
+var previousOp = "";
+var currentOp = "";
+var operation = undefined;
 
 
-btn.forEach(button => {
-    button.addEventListener("click" , () => {
-        if (button.innerHTML === "%" || "/" || "X" || "-" || "+") {
-            addOperation(button.innerHTML)
-         }
-    } )
-});
 
-addOperation = (theOp) => {
-    console.log(theOp);
+btnNum.forEach(button => {
+    button.addEventListener("click", () => {
+        if (button.innerHTML === "."){
+            if 
+        }
+        addNumber(button.innerHTML);
+    })
+})
+
+btnOp.forEach(button => {
+    button.addEventListener("click", () => {
+        opSelection();
+        addOperand(button.innerHTML);
+    })
+})
+
+btnClear.addEventListener("click" , () => {
+    clear()
+})
+
+btnDel.addEventListener("click", () => {
+    currDisplay.value = currDisplay.value.slice(0, -1);
+})
+
+btnClear.addEventListener("click" , () => {
+    clear()
+})
+
+btnEqual.addEventListener("click", () => {
+    calculate()
+    displaySwitch()
+})
+
+updateDisplay = () => {
+    currDisplay = currentOp;
+    prevDisplay = previousOp;
 }
+
+clear = () => {
+    prevDisplay.value = "";
+    currDisplay.value = "";
+}
+
+addNumber = (num) => {
+    currDisplay.value += num;
+}
+
+addOperand = (oper) => {
+    previousOp = currentOp
+    currentOp = "";
+}
+
+// calculate = () => {
+    
+// }
+
+
